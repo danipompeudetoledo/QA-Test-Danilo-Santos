3722aad0d586e1a63d14832c6ec8ffd125d95d0e
danilodias.1020@gmail.com <danilosantos.itech@gmail.com>
2022-08-05 16:45:03 -0300

    ricochet

--- cypress/integration/shop.spec.js
+++ cypress/integration/shop.spec.js
@@ -12,68 +12,51 @@ describe("Shopping", function () {
       shopPage.sortingHighToLow();
     });
 
-  context("Validate the working of add to cart functionality", function () {
-
-    it("Product should be successfuly displayed in the shop Cart", function () {
-      shopPage.go();
-      shopPage.addingToBasket();
-      shopPage.viewBasket()
- 
-    });
-  context("Validate the product buy flow", function () {
-
-    const user = {
-        firstName:'João',
-        lastName:'Paulo',
-        company:'Amazon',
-        email:'jped@gmail.com',
-        phone :'551145875452',
-        address: 'Paulista Avenue',
-        city: 'São Paulo',
-        postcode:'06395450',
-        password:'D@nilo$1990B#'
-
-    }
-    it.only("Verify that user can buy products that were added to his cart", function () {
+    context("Validate the working of add to cart functionality", function () {
+      it("Product should be successfuly displayed in the shop Cart", function () {
         shopPage.go();
         shopPage.addingToBasket();
-        shopPage.viewBasket()
-        shopPage.checkout()
-        shopPage.fillBillinDetails(user)
-     });
-  
-      it("Verify... ", function () {
-        shopPage.go();
-        shopPage.sortingHighToLow();
+        shopPage.viewBasket();
       });
-  
-    })
-  
+      context("Validate the product buy flow", function () {
+        const user = {
+          firstName: "João",
+          lastName: "Paulo",
+          company: "Amazon",
+          email: "jped@gmail.com",
+          phone: "551145875452",
+          address: "Paulista Avenue",
+          city: "São Paulo",
+          postcode: "06395450",
+          password: "D@nilo$1990B#",
+        };
+        it.only("Verify that user can buy products that were added to his cart", function () {
+          shopPage.go();
+          shopPage.addingToBasket();
+          shopPage.viewBasket();
+          shopPage.checkout();
+          shopPage.fillBillinDetails(user);
+        });
 
-
-
-
-
-
-
+        it("Verify... ", function () {
+          shopPage.go();
+          shopPage.sortingHighToLow();
+        });
+      });
+    });
   });
-  
-
-  })
- 
 });
-Cypress._.times(500, index => {
+Cypress._.times(500, (index) => {
+  it(`selects ${index} out of 10`, () => {
+    cy.visit("/shop");
+    cy.get(
+      ".price_slider_wrapper, .ui-slider-handle ui-corner-all ui-state-default"
+    )
+      .invoke("val", index)
+      .should("have.value", index)
+      .trigger("change");
 
-  it(`selects ${index} out of 10`, () =>{
-      cy.visit('/shop')
-      cy.get('.price_slider_wrapper, .ui-slider-handle ui-corner-all ui-state-default')
-          .invoke('val', index)
-          .should('have.value', index)
-          .trigger('change')
-  
-          cy.contains('.from','-','.to', index)
-          cy.wait(500)
-  
-  })
-  
-  })
+    cy.contains(".from", "-", ".to", index);
+    cy.wait(500);
+  });
+});
